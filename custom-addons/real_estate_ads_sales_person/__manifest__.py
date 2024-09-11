b4c14fbb14146972c1cf45856471d4ea166217dd
Altantuya <45263204+tuyakaa@users.noreply.github.com>
2024-09-11 12:05:50 +0800

    11.2

---
+++ custom-addons/real_estate_ads_sales_person/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Real Estate Ads - Sales Person",
+    "version" : "1.0",
+    "website": "https://www.odoo17_b.com",
+    "author": "Buynaa",
+    "description": """
+        Show real estate properties linked to a sales person
+    """,
+    "category": "Sales",
+    'depends': ["real_estate_ads", "base"],
+    "data": [
+        "views/res_users.xml"
+    ],
+    "installable": True,
+    "license": "GPL-3"
+}
